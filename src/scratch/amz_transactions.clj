bc1e7d86b08b2d62f72028f21ccdbc873a1c00f9
Antonio A <peluko@peluko.net>
2025-11-10 10:22:37 +0000

    wip

--- src/scratch/amz_transactions.clj
+++ src/scratch/amz_transactions.clj
@@ -3,7 +3,17 @@
    [clj-http.client :as client]
    [clojure.edn :as edn]
    [tick.core :as t]
+   [malli.core :as m]
+   [malli.transform :as mt]
+   [malli.error :as me]
+   [malli.experimental.time :as met]
+   [malli.registry :as mr]
    [clj-reload.core]))
+
+(mr/set-default-registry!
+ (mr/composite-registry
+  (m/default-schemas)
+  (met/schemas)))
 
 (def amz-mkts {:ie "A28R8C7NBKEWEA" ;; Ireland
                :es "A1RKKUPIHCS9HS" ;; Spain
@@ -38,14 +48,21 @@
                      :body
                      :access_token)))
 
-(defn api-get [endpoint]
+(def transaction-schema [:map
+                         [:transactionType :string]])
+
+(defn api-get [endpoint params]
   (let [url (str "https://sellingpartnerapi-eu.amazon.com" endpoint)
         user-agent "PSManager/0.6 (Language=Clojure/1.12/3/1577; Platform=Ubuntu/22.04)"
         now (t/format (t/formatter :iso-zoned-date-time) (t/zoned-date-time))]
     (try
-      (client/get url {:headers {:user-agent user-agent
-                                 :x-amz-access-token bearer-token
-                                 :x-amz-date "20251103T183101"}})
+      (->> (client/get url {:headers {:user-agent user-agent
+                                      :x-amz-access-token bearer-token
+                                      :x-amz-date now}
+                            :query-params params
+                            :accept :json
+                            :as :json})
+           :body)
       (catch Exception e
         e))))
 
@@ -53,9 +70,56 @@
 
   (clj-reload.core/reload)
 
-  (let [endpoint (str "/catalog/2022-04-01/items/B0FRY77BJN" #_"?marketplaceIds=" #_(:es amz-mkts))]
-    (api-get endpoint))
+  (def transactions (let [endpoint "/finances/2024-06-19/transactions"
+                          query-params {:postedAfter "2025-11-01"
+                                        :postedBefore "2025-11-08"}]
+                      (->> (api-get endpoint query-params)
+                           :payload
+                           :transactions)))
 
+  (let [descriptions (map #(:description %) transactions)]
+    (distinct descriptions))
+
+  (first transactions)
+
+  (do
+    (def transaction-schema (let [transaction-type-enum [:enum
+                                                         "Shipment"
+                                                         "ProductAdsPayment"
+                                                         "ServiceFee"
+                                                         "Refund"
+                                                         "Transfer"]
+                                  description-enum [:enum
+                                                    "Order Payment"
+                                                    "ProductAdsPayment"
+                                                    "Subscription"
+                                                    "FBACustomerReturn"
+                                                    "Refund"
+                                                    "FBAStorageBilling"
+                                                    "FBARemoval"
+                                                    "FBALongTermStorageBilling"
+                                                    "Disbursement"
+                                                    "FBAPostInboundTransportation"
+                                                    "FBAShipmentInjectionFulfillmentFee"]
+                                  related-identier-name-enum [:or :string [:enum ;; for reference only we list the relevant ones
+                                                                           "ORDERID"
+                                                                           "SHIPMENTID"
+                                                                           "SETTLEMENTID"]]
+                                  transaction-status-enum [:enum "RELEASED" "DEFERRED", "DEFERRED_RELEASED"]]
+                              [:map
+                               [:transactionType transaction-type-enum]
+                               [:description description-enum]
+                               [:relatedIdentifiers [:vector [:map [:relatedIdentifierName related-identier-name-enum] [:relatedIdentifierValue :string]]]]
+                               [:transactionStatus transaction-status-enum]
+                               [:postedDate [:time/instant {:pattern "yyyy-MM-ddThh:mm:ssZ"}]]]))
+
+    (def transactions-schema [:vector transaction-schema])
+
+    (try
+      (-> transactions-schema
+          (m/coerce transactions mt/strip-extra-keys-transformer))
+      (catch Exception e
+        (-> e ex-data :data :explain me/humanize))))
 
   :efc
   )
